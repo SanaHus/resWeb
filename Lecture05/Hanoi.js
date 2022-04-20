3a67b2f59ec00a013253f7ba250689526110c412
PC7213_24\YUHAN <cyu0304@gmail.com>
2022-04-20 15:45:14 +0900

    OnClick 구현함 ㄹㅇㅋㅋ

--- Lecture05/Hanoi.js
+++ Lecture05/Hanoi.js
@@ -1,1 +1,11 @@
-//나는 잘 모르겠다
+function clickStage00()  {
+    alert('안녕하세요');
+  }
+
+  function clickStage01()  {
+    alert('안녕하세요');
+  }
+
+  function clickStage02()  {
+    alert('안녕하세요');
+  }
