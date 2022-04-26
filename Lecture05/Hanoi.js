538de93b81e797e0e5423a5405d4cc9d70a0e57b
Serleim <cyu0304@gmail.com>
2022-04-26 22:21:48 +0900

    옮기기 성공했어요 엘프님!!

--- Lecture05/Hanoi.js
+++ Lecture05/Hanoi.js
@@ -1,11 +1,31 @@
+var grab = false;
+var grabData = null;
+
 function clickStage00()  {
-    alert('안녕하세요');
+  if(!grab) {
+    grabData = document.getElementById("p1");
+    document.getElementById("p1").remove();
+    grab = true;
   }
+  else if(grab){
+    document.getElementById("stage0").prepend(grabData);
+    grabData = null;
+    grab = false;
+  }
+}
 
   function clickStage01()  {
-    alert('안녕하세요');
+    if(grab) {
+      document.getElementById("stage1").appendChild(grabData);
+      grab = false;
+    }
+    //alert('안녕하세요');
   }
 
   function clickStage02()  {
-    alert('안녕하세요');
+    if(grab) {
+      document.getElementById("stage2").appendChild(grabData);
+      grab = false;
+    }
+    //alert('안녕하세요');
   }
